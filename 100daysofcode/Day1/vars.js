ee994eff7910e880f922f918f51c30cf1cacccda
izhan <mohdizhanali@gmail.com>
2022-12-12 08:15:06 +0530

    fixed dir structure

---
+++ 100daysofcode/Day1/vars.js
@@ -0,0 +1,16 @@
+//Variables
+
+//Two Types of variables
+var myNum = 10 //old metyhod
+let msg = "Hi" //new 
+myNum++
+console.log(myNum)
+
+msg = "Hello"
+
+//Constant
+
+const pi = 2.14
+console.log(pi)
+
+console.log(typeof(pi))
