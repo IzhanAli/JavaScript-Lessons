328215cd97956aec8e6fe69c9cba8a6c508693b5
izhan <mohdizhanali@gmail.com>
2022-12-29 01:25:02 +0530

    strings

---
+++ 100daysofcode/Day5/stringmethods.js
@@ -0,0 +1,25 @@
+let user = "Izhan"
+console.log(user.length)
+user = "     Izhan     "
+// console.log(user.trim().length)
+user = user.toUpperCase()
+user = user.toLowerCase()
+
+let newstring = "lorem ipsum dopes"
+console.log(newstring.slice(-4))
+
+// typeconversoion
+// string to Number
+newstring = +"55"
+console.log(typeof newstring)
+newstring = 55+""
+console.log(typeof newstring)
+
+//concatenation
+
+first = "helolo"
+lasdt = "world"
+
+console.log(first+lasdt)
+
+
